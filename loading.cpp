16bab260bcc877967381e010ee4c50b7af77da54
elisatisya <elisatisya1@gmail.com>
2024-11-27 21:14:17 +0700

    revisi loading fix

--- loading.cpp
+++ loading.cpp
@@ -3,14 +3,73 @@
 #include <windows.h>
 using namespace std;
 
+void teks(){
+    string tulisan[4] = {   " ___  ___  _       _    __  __    _    _____ ",
+                            "/ __|| __|| |     /_\\  |  \\/  |  /_\\  |_   _|",
+                            "\\__ \\| _| | |__  / _ \\ | |\\/| | / _ \\   | |   ",
+                            "|___/|___||____|/_/ \\_\\|_|  |_|/_/ \\_\\  |_|   "};
+
+    string tulisan1[4] = {  " ___     _    _____    _    _  _   ___ ",
+                            "|   \\   /_\\  |_   _|  /_\\  | \\| | / __|",
+                            "| |) | / _ \\   | |   / _ \\ | .` || (_ |",
+                            "|___/ /_/ \\_\\  |_|  /_/ \\_\\|_|\\_| \\___|"};
+
+    for(int i = 0; i < 4; i++){
+        mvprintw(LINES / 2 - 5 + i, COLS / 2 - 23, "%s", tulisan[i].c_str());
+        mvprintw(LINES / 2 + 1 + i, COLS / 2 - 20, "%s", tulisan1[i].c_str());
+
+        refresh();
+        Sleep(200);
+    }
+}
+
+void teks1(){
+    string tulisan[9] = {   "   _     _      _     _      _     _      _     _   ",
+                            "  (c).-.(c)    (c).-.(c)    (c).-.(c)    (c).-.(c)  ",
+                            "   / ._. \\      / ._. \\      / ._. \\      / ._. \\   ",
+                            " __\\( Y )/__  __\\( Y )/__  __\\( Y )/__  __\\( Y )/__ ",
+                            "(_.-/'-'\\-._)(_.-/'-'\\-._)(_.-/'-'\\-._)(_.-/'-'\\-._)",
+                            "   || R ||      || A ||      || W ||      || R ||   ",
+                            " _.' `-' '._  _.' `-' '._  _.' `-' '._  _.' `-' '._ ",
+                            "(.-./`-`\\.-.)(.-./`-'\\.-.)(.-./`-'\\.-.)(.-./`-`\\.-.)",
+                            " `-'     `-'  `-'     `-'  `-'     `-'  `-'     `-' "};
+
+    for(int i = 0; i < 9; i++){
+        mvprintw(LINES / 2 - 4 + i, COLS / 2 - 26, "%s", tulisan[i].c_str());
+
+        refresh();
+        Sleep(100);
+    }
+}
+
+void penghapus(){
+    const char p1[] = " ";
+
+    for(int i = 30; i <= COLS - 30; i++){
+        mvprintw(LINES / 2 - 5, i, p1);
+        mvprintw(LINES / 2 - 4, i, p1);
+        mvprintw(LINES / 2 - 3, i, p1);
+        mvprintw(LINES / 2 - 2, i, p1);
+        mvprintw(LINES / 2 - 1, i, p1);
+        mvprintw(LINES / 2, i, p1);
+        mvprintw(LINES / 2 + 1, i, p1);
+        mvprintw(LINES / 2 + 2, i, p1);
+        mvprintw(LINES / 2 + 3, i, p1);
+        mvprintw(LINES / 2 + 3, i, p1);
+        mvprintw(LINES / 2 + 4, i, p1);
+        mvprintw(LINES / 2 + 5, i, p1);
+
+        refresh();
+        Sleep(10);
+    }
+}
+
 int main(){
     initscr();
     curs_set(0);
     start_color();
     
     string titik = "....";
-    string huruf1 = "SELAMAT DATANG KAWAN";
-    string huruf2 = "semoga menang xixi";
 
     int a = 0;
     int b = 0;
@@ -21,38 +80,32 @@ int main(){
     init_pair(3, COLOR_RED, COLOR_BLACK);
 
     
-    for(int i=25; i<= COLS - 25; i++){
+    for(int i=20; i<= COLS - 25; i++){
         mvprintw(5,i, "=");
         mvprintw(LINES - 5,i, "=");
         refresh();
         Sleep(50);
     }
     for(int i=5; i <= LINES - 5; i++ ){
-        mvprintw(i,25, "#");
+        mvprintw(i,20, "#");
         mvprintw(i, COLS - 25, "#");
         refresh();
         Sleep(50);
     }
 
-    for (int i = 1; i <= 50; i++){
-        if(b == 20) b = 0;
-        if(b == 0) mvprintw(10,52, " ");
+    attron(COLOR_PAIR(1));
+    teks();
 
-        attron(COLOR_PAIR(3));
+    Sleep(200);
 
-        mvprintw(LINES/2 - 1, COLS/2 - 11 + b, "%c", huruf1[b]);
-        if(c == 18) c = 0;
-        if(c == 0) mvprintw(13,52, " ");
-        mvprintw(LINES/2 + 1, COLS/2 - 10 + c, "%c", huruf2[c]);
-        attroff(COLOR_PAIR(3));
+    penghapus();
 
-        refresh();
-        Sleep(50);
-        b++;
-        c++;
-    }
-    mvprintw(LINES/2 - 1, COLS/2 - 11,"                                 ");
-    mvprintw(LINES/2 + 1, COLS/2 - 10,"                                 ");
+    attron(COLOR_PAIR(2));
+    teks1();
+
+    Sleep(500);
+
+    penghapus();
 
     for (int i = 1; i <= 100; i++){
         if(a == 4) a = 0;
@@ -76,4 +129,6 @@ int main(){
     clear();
 
     endwin();
+
+    
 }
